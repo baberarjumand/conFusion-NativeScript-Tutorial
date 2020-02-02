e18b3d4afdc3f62b5895902f34ec1d477649f0a7
baberarjumand <baber.arjumand@gmail.com>
2020-02-02 13:51:06 +0300

    completed exercise 7.7, nativescript nav p2, added home component and updated sidebar

--- app/app.routing.ts
+++ app/app.routing.ts
@@ -6,14 +6,16 @@ import { Routes } from "@angular/router";
 // import { ItemDetailComponent } from "./item/item-detail.component";
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
     // { path: "", redirectTo: "/items", pathMatch: "full" },
     // { path: "items", component: ItemsComponent },
     // { path: "item/:id", component: ItemDetailComponent },
-    { path: "", redirectTo: "/menu", pathMatch: "full" },
+    { path: "", redirectTo: "/home", pathMatch: "full" },
+    { path: "home", component: HomeComponent },
     { path: "menu", component: MenuComponent },
-    { path: 'dishdetail/:id', component: DishdetailComponent }
+    { path: 'dishdetail/:id',     component: DishdetailComponent }
 ];
 
 @NgModule({
